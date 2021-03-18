43024973e425de4f73b6e3af896f7ca2b5077bcf
Rednir <davran.dilshat@gmail.com>
2021-03-18 19:41:02 +0000

    serialize thumbnail data

--- src/Modules/Voice/YoutubeVideoMetadata.cs
+++ src/Modules/Voice/YoutubeVideoMetadata.cs
@@ -1,4 +1,4 @@
-
+using System.Collections.Generic;
 
 namespace wow2.Modules.Voice
 {
@@ -12,6 +12,15 @@ namespace wow2.Modules.Voice
         public int? like_count { get; set; }
         public int? dislike_count { get; set; }
 
-        //public List<MetadataThumbnails> thumbnails { get; set; }
+        public List<YoutubeVideoMetadataThumbnails> thumbnails { get; set; }
+    }
+
+    public class YoutubeVideoMetadataThumbnails
+    {
+        public string url { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+        public string resolution { get; set; }
+        public string id { get; set; }
     }
 }
