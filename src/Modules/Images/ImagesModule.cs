a49d0b004eeb73a728f920fb35d47bbe28566272
Rednir <davran.dilshat@gmail.com>
2021-03-19 20:13:05 +0000

    save image to stream instead of disk

--- src/Modules/Images/ImagesModule.cs
+++ src/Modules/Images/ImagesModule.cs
@@ -1,11 +1,14 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.IO;
 using System.Threading.Tasks;
+using Discord;
 using Discord.Commands;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Processing;
 using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.Formats.Jpeg;
 using ExtentionMethods;
 
 namespace wow2.Modules.Images
@@ -16,27 +19,41 @@ namespace wow2.Modules.Images
     [Summary("For creating and editing images.")]
     public class ImagesModule : ModuleBase<SocketCommandContext>
     {
-        public static readonly string QuoteResultPath = $"{System.IO.Path.GetTempPath()}/wow2quoteresult.jpg";
-
         [Command("quote")]
         [Alias("quotes")]
         public async Task QuoteAsync(string quote, string author = null)
         {
             var listOfTemplatePaths = Directory.EnumerateFiles("res/quotetemplates");
             string templateToUsePath = listOfTemplatePaths.ElementAt(new Random().Next(listOfTemplatePaths.Count() - 1));
+
             if (author == null)
                 author = Path.GetFileNameWithoutExtension(templateToUsePath);
 
-            using (Image image = Image.Load(templateToUsePath))
+            using (SixLabors.ImageSharp.Image image = SixLabors.ImageSharp.Image.Load(templateToUsePath))
             {
+                MemoryStream fileStreamForImage = new MemoryStream();
+
                 Size imageSize = image.Size();
                 int quoteXPos = (imageSize.Width / 2) + (imageSize.Width / 32);
                 int quoteYPos = (imageSize.Height / 2) - (imageSize.Height / 6) - (quote.Length / 4);
 
-                image.Mutate(x => x.DrawText($"\"{quote.Wrap(40)}\"\n\n - {author}", ImagesModuleFonts.QuoteTextFont, Color.LightGrey, new PointF(quoteXPos, quoteYPos)));
+                image.Mutate(x => x.DrawText($"\"{quote.Wrap(40)}\"\n\n - {author}", ImagesModuleFonts.QuoteTextFont, SixLabors.ImageSharp.Color.LightGrey, new PointF(quoteXPos, quoteYPos)));
 
-                await image.SaveAsJpegAsync(QuoteResultPath);
-                await Context.Channel.SendFileAsync(QuoteResultPath);
+                await image.SaveAsync(fileStreamForImage, new JpegEncoder());
+                fileStreamForImage.Position = 0;
+                await Context.Channel.SendFileAsync(fileStreamForImage, "wow2quoteresult.jpg");
+            }
+        }
+
+        [Command("quote-list")]
+        [Alias("quotes-list", "quotelist")]
+        public async Task QuoteListAsync()
+        {
+            var listOfTemplatePaths = Directory.EnumerateFiles("res/quotetemplates");
+
+            var listOfFieldBuilders = new List<EmbedFieldBuilder>();
+            foreach (string path in listOfTemplatePaths)
+            {
             }
         }
     }
