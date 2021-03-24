f874b26838080af1cf98e3da6b3be7e6b0d255ac
Rednir <davran.dilshat@gmail.com>
2021-03-24 17:51:51 +0000

    make naming clearer

--- src/Modules/Voice/VoiceModule.cs
+++ src/Modules/Voice/VoiceModule.cs
@@ -19,7 +19,7 @@ namespace wow2.Modules.Voice
     [Name("Voice")]
     [Group("vc")]
     [Alias("voice")]
-    [Summary("For playing Youtube audio in a voice channel.")]
+    [Summary("For playing Youtube/Twitch audio in a voice channel.")]
     public class VoiceModule : ModuleBase<SocketCommandContext>
     {
         [Command("list")]
