278b5cba2a1b2f15e528d38d7f9e244b65b974fb
Rednir <davran.dilshat@gmail.com>
2021-03-24 18:20:32 +0000

    fix wrong text when toggling auto np

--- src/Modules/Voice/VoiceModule.cs
+++ src/Modules/Voice/VoiceModule.cs
@@ -166,7 +166,7 @@ namespace wow2.Modules.Voice
         {
             DataManager.GetVoiceConfigForGuild(Context.Guild).IsAutoNpOn = !DataManager.GetVoiceConfigForGuild(Context.Guild).IsAutoNpOn;
             await DataManager.SaveGuildDataToFileAsync(Context.Guild.Id);
-            await GenericMessenger.SendSuccessAsync(Context.Channel, $"Auto np is now `{(DataManager.GetVoiceConfigForGuild(Context.Guild).IsAutoNpOn ? "on" : "off")}` for keyword responses.");
+            await GenericMessenger.SendSuccessAsync(Context.Channel, $"Auto execution of `np` is turned `{(DataManager.GetVoiceConfigForGuild(Context.Guild).IsAutoNpOn ? "on" : "off")}`");
         }
 
         private async Task DisplayCurrentlyPlayingRequestAsync()
