6efe24dc622b4b0e134b982f81b621876b41991f
Rednir <davran.dilshat@gmail.com>
2021-03-31 21:41:37 +0100

    add warn command placeholder

--- src/Modules/Moderator/ModeratorModule.cs
+++ src/Modules/Moderator/ModeratorModule.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Threading.Tasks;
+using Discord;
 using Discord.WebSocket;
 using Discord.Commands;
+using wow2.Verbose;
 
 namespace wow2.Modules.Moderator
 {
@@ -10,11 +13,22 @@ namespace wow2.Modules.Moderator
     [Summary("For using tools to manage the server.")]
     public class ModeratorModule : ModuleBase<SocketCommandContext>
     {
+        [Command("warn")]
+        [Summary("Sends a user a DM with an optional warning message. Requires the 'Kick Members' permission.")]
+        [RequireUserPermission(GuildPermission.KickMembers)]
+        public async Task WarnAsync(SocketGuildUser user, string message = "No reason given")
+        {
+            IDMChannel dmChannel = await user.GetOrCreateDMChannelAsync();
+            await GenericMessenger.SendInfoAsync(dmChannel, "Warniing");
+        }
+
         [Command("mute")]
         [Alias("silence")]
-        [Summary("Temporarily disables a user's permission to speak")]
-        public async Task MuteAsync(SocketUser userToMute)
+        [Summary("Temporarily disables a user's permission to speak. Requires the 'Kick Members' permission.")]
+        [RequireUserPermission(GuildPermission.KickMembers)]
+        public async Task MuteAsync(SocketUser user, string time, string message = "No reason given.")
         {
+            throw new NotImplementedException(); 
         }
     }
 }
