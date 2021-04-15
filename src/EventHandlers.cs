889077ed8f6d4eb714a3001c9816a9226c89ca0a
Rednir <davran.dilshat@gmail.com>
2021-04-15 20:09:42 +0100

    CAN THIS PLEASE WORK???

--- src/EventHandlers.cs
+++ src/EventHandlers.cs
@@ -65,9 +65,9 @@ namespace wow2
                 Logger.LogException(logMessage.Exception);
 
                 // Return as client will immediately reconnect after this exception. 
-                if ((logMessage.Exception is GatewayReconnectException ||
-                    logMessage.Exception is WebSocketClosedException ||
-                    logMessage.Exception is WebSocketException) &&
+                if ((logMessage.Exception.InnerException is GatewayReconnectException ||
+                    logMessage.Exception.InnerException is WebSocketClosedException ||
+                    logMessage.Exception.InnerException is WebSocketException) &&
                     !Program.IsDebug)
                     return;
 
