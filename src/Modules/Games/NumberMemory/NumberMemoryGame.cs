f6f90d0b8b7dfbfaefc856acd0cd6afe0f8c9a39
Rednir <davran.dilshat@gmail.com>
2021-04-18 11:32:35 +0100

    add game start

--- src/Modules/Games/NumberMemory/NumberMemoryGame.cs
+++ src/Modules/Games/NumberMemory/NumberMemoryGame.cs
@@ -19,10 +19,19 @@ namespace wow2.Modules.Games.NumberMemory
         public static async Task StartGame(SocketCommandContext context)
         {
             var config = GetConfigForGuild(context.Guild);
+
+            config.IsGameStarted = true;
+            config.HighestNumberOfDigits = 0;
+
+            await new InfoMessage(
+                description: "When the number disappears, try retype the number. Don't start typing before the message gets deleted!",
+                title: $"Number memory has started for {context.User.Mention}")
+                    .SendAsync(context.Channel);
         }
 
         private static async Task EndGameAsync(NumberMemoryConfig config)
         {
+            config.IsGameStarted = false;
         }
 
         private static NumberMemoryConfig GetConfigForGuild(SocketGuild guild)
