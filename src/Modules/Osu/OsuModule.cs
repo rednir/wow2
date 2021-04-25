5be78f283d1cd58e6dd916528f1d39f751ba28a8
Rednir <davran.dilshat@gmail.com>
2021-04-25 11:09:29 +0100

    authorize http cilent

--- src/Modules/Osu/OsuModule.cs
+++ src/Modules/Osu/OsuModule.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Discord.Commands;
 using wow2.Verbose.Messages;
@@ -12,10 +15,15 @@ namespace wow2.Modules.Osu
     [Summary("Integrations with the osu!api")]
     public class OsuModule : ModuleBase<SocketCommandContext>
     {
-        public HttpClient HttpClient = new()
+        private readonly HttpClient HttpClient = new()
         {
-            BaseAddress = new Uri("https://osu.ppy.sh/api/v2")
+            BaseAddress = new Uri("https://osu.ppy.sh/")
         };
+
+        public OsuModule()
+        {
+            _ = InitializeHttpClient();
+        }
 
         [Command("subscribe")]
         [Alias("sub")]
@@ -23,6 +31,24 @@ namespace wow2.Modules.Osu
         public async Task SubscribeAsync(string user)
         {
             throw new NotImplementedException();
+        }
+
+        private async Task InitializeHttpClient()
+        {
+            var tokenRequestParams = new Dictionary<string, string>()
+            {
+                {"client_id", DataManager.Secrets.OsuClientId},
+                {"client_secret", DataManager.Secrets.OsuClientSecret},
+                {"grant_type", "client_credentials"},
+                {"scope", "public"}
+            };
+            var tokenRequestResponse = await HttpClient
+                .PostAsync("oauth/token", new FormUrlEncodedContent(tokenRequestParams))
+                .Result.Content.ReadFromJsonAsync<Dictionary<string, object>>();
+
+            HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+                "Bearer", tokenRequestResponse["access_token"].ToString());
+            HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
     }
 }
