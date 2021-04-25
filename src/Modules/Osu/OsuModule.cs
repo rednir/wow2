5f34f9143c7a6dda57e019c0fc98f084f5438f3b
Rednir <davran.dilshat@gmail.com>
2021-04-25 12:08:26 +0100

    add user command

--- src/Modules/Osu/OsuModule.cs
+++ src/Modules/Osu/OsuModule.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Net;
 using System.Collections.Generic;
 using System.Text.Json;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
+using Discord;
 using Discord.Commands;
 using wow2.Verbose;
 using wow2.Data;
@@ -24,6 +26,32 @@ namespace wow2.Modules.Osu
         static OsuModule()
         {
             _ = InitializeHttpClient();
+        }
+
+        [Command("user")]
+        [Alias("player")]
+        [Summary("Get some infomation about a user.")]
+        public async Task UserAsync(string user)
+        {
+            UserData userData = await GetUserAsync(user);
+            var embedBuilder = new EmbedBuilder()
+            {
+                Author = new EmbedAuthorBuilder()
+                {
+                    Name = $"{userData.username} | #1234",
+                    IconUrl = userData.avatar_url,
+                    Url = $"https://osu.ppy.sh/users/{userData.id}"
+                },
+                //Title = "User Overview",
+                Description = $"**Performance:** {userData.statistics.pp}\n**Accuracy:** {Math.Round(userData.statistics.hit_accuracy)}%\n**Time Played:** {userData.statistics.play_time / 3600}h",
+                Footer = new EmbedFooterBuilder()
+                {
+                    Text = $"Joined: {DateTime.Parse(userData.join_date)}"
+                },
+                ImageUrl = userData.cover_url,
+                Color = Color.LightGrey
+            };
+            await ReplyAsync(embed: embedBuilder.Build());
         }
 
         [Command("subscribe")]
@@ -60,6 +88,18 @@ namespace wow2.Modules.Osu
             HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
                 "Bearer", tokenRequestResponse["access_token"].ToString());
             HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        private static async Task<UserData> GetUserAsync(string user)
+        {
+            var response = await HttpClient.GetAsync($"api/v2/users/{user}");
+
+            // If `user` is a username, the client will be redirected, losing
+            // its headers. So another request will need to be made. 
+            if (response.StatusCode == HttpStatusCode.Unauthorized)
+                response = await HttpClient.GetAsync(response.RequestMessage.RequestUri);
+
+            return await response.Content.ReadFromJsonAsync<UserData>();
         }
     }
 }
