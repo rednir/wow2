853c73633788f49749b66401c8030b4ac814ab66
Rednir <davran.dilshat@gmail.com>
2021-04-25 16:36:34 +0100

    handle no bot token

--- src/Data/DataManager.cs
+++ src/Data/DataManager.cs
@@ -52,8 +52,14 @@ namespace wow2.Data
         public static async Task LoadSecretsFromFileAsync()
         {
             string fullPath = AppDataDirPath + "/secrets.json";
-            if (File.Exists(fullPath))
-                Secrets = JsonSerializer.Deserialize<Secrets>(File.ReadAllText(fullPath));
+            if (!File.Exists(fullPath))
+            {
+                Logger.Log($"Couldn't find a secrets file at {fullPath}, so one was created. You MUST have your Discord bot token at the very least in the secrets file in order to run this program.", LogSeverity.Warning);
+                await File.WriteAllTextAsync(fullPath, JsonSerializer.Serialize(Secrets, SerializerOptions));
+                Environment.Exit(-1);
+            }
+            
+            Secrets = JsonSerializer.Deserialize<Secrets>(File.ReadAllText(fullPath));
 
             // Always rewrite file, just in case there are new properties.
             await File.WriteAllTextAsync(fullPath, JsonSerializer.Serialize(Secrets, SerializerOptions));
