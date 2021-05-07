0cc962143288a08ed5b056d697373012dc4b221d
Rednir <davran.dilshat@gmail.com>
2021-05-07 17:16:57 +0100

    make tests into own methods

--- src/Modules/Dev/Tests.cs
+++ src/Modules/Dev/Tests.cs
@@ -19,192 +19,212 @@ namespace wow2.Modules.Dev
         public static readonly Dictionary<string, Func<ICommandContext, Task>> TestList = new()
         {
             {
-                "messages", async (context) =>
-                {
-                    await new SuccessMessage("This is a success message.", "Success").SendAsync(context.Channel);
-                    await new InfoMessage("This is an info message.", "Info").SendAsync(context.Channel);
-                    await new WarningMessage("This is a warning message.", "Warning").SendAsync(context.Channel);
-                    await new ErrorMessage("This is an error message.", "Error").SendAsync(context.Channel);
-                    await new GenericMessage("This is a response message.", "Response").SendAsync(context.Channel);
-
-                    var listOfFieldBuilders = new List<EmbedFieldBuilder>();
-                    for (int i = 0; i < 46; i++)
-                    {
-                        listOfFieldBuilders.Add(new EmbedFieldBuilder()
-                        {
-                            Name = $"Field title {i}",
-                            Value = "This is some description text."
-                        });
-                    }
-                    await new GenericMessage("This is a response message with fields.", "Fields", listOfFieldBuilders, 2).SendAsync(context.Channel);
-                }
+                "messages",
+                MessagesTest
             },
             {
-                "aliases", async (context) =>
-                {
-                    var config = MainModule.GetConfigForGuild(context.Guild);
-                    const string aliasName = "testing_alias";
-
-                    await ExecuteAsync(context,
-                        $"alias {aliasName} \"{config.CommandPrefix} help\"");
-                    await AssertAsync(context, new()
-                    {
-                        {"key exists in dictionary", config.AliasesDictionary.ContainsKey(aliasName)},
-                        {"check definition", config.AliasesDictionary[aliasName] == "help"}
-                    });
-
-                    await ExecuteAsync(context,
-                        $"alias {aliasName}");
-                    await AssertAsync(context,
-                        "alias has been removed", !config.AliasesDictionary.ContainsKey(aliasName));
-                }
+                "aliases",
+                AliasesTest
             },
             {
-                // TODO: These Task.Delays are a bit of a hacky workaround.
-                //       Find some way to reliably wait until the command finishes with timeout.
-                "voice", async (context) =>
-                {
-                    var config = VoiceModule.GetConfigForGuild(context.Guild);
-
-                    await ExecuteAsync(context,
-                        "vc clear",
-                        "vc skip",
-                        "vc join");
-                    await DelayAsync(context, 3000);
-                    await AssertAsync(context, new()
-                    {
-                        {"song request queue is empty", config.CurrentSongRequestQueue.Count == 0},
-                        {"nothing is playing", config.CurrentlyPlayingSongRequest == null},
-                        {"audio client has connected", !VoiceModule.CheckIfAudioClientDisconnected(config.AudioClient)}
-                    });
-
-                    await ExecuteAsync(context,
-                        "vc add never gonna give you up",
-                        "vc add https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-                        "vc add \"me at the zoo\"");
-                    await DelayAsync(context, 10000);
-                    await ExecuteAsync(context,
-                        "vc list");
-                    await AssertAsync(context, new()
-                    {
-                        {"currently playing is not null", config.CurrentlyPlayingSongRequest != null},
-                        {"song request queue is correct length", config.CurrentSongRequestQueue.Count == 2}
-                    });
-
-                    var firstCurrentlyPlayingRequest = config.CurrentlyPlayingSongRequest;
-
-                    await ExecuteAsync(context,
-                        "vc skip");
-                    await AssertAsync(context, new()
-                    {
-                        {"check next request is playing", config.CurrentlyPlayingSongRequest != firstCurrentlyPlayingRequest}
-                    });
-
-                    await ExecuteAsync(context,
-                        "vc clear");
-                    await AssertAsync(context,
-                        "song request queue is empty", config.CurrentSongRequestQueue.Count == 0);
-
-                    await ExecuteAsync(context,
-                        "vc leave");
-                    await AssertAsync(context,
-                        "audio client has disconnected", VoiceModule.CheckIfAudioClientDisconnected(config.AudioClient));
-                }
+                "voice",
+                VoiceTest
             },
             {
-                "voice-queue", async (context) =>
-                {
-                    var config = VoiceModule.GetConfigForGuild(context.Guild);
-                    const string queueName = "testing-queue";
-
-                    await ExecuteAsync(context,
-                        $"vc pop-queue {queueName}",
-                        "vc clear");
-                    await AssertAsync(context, new()
-                    {
-                        {"testing queue does not exist", !config.SavedSongRequestQueues.ContainsKey(queueName)},
-                        {"song request queue is empty", config.CurrentSongRequestQueue.Count == 0}
-                    });
-
-                    await ExecuteAsync(context,
-                        "vc add https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-                        "vc add https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-                    await DelayAsync(context, 10000);
-                    await AssertAsync(context,
-                        "song request queue is correct length", config.CurrentSongRequestQueue.Count == 2);
-
-                    await ExecuteAsync(context,
-                        $"vc save {queueName}");
-                    await AssertAsync(context, new()
-                    {
-                        {"testing queue exists in saved queues", config.SavedSongRequestQueues.ContainsKey(queueName)},
-                        {"correct number of songs in testing queue", config.SavedSongRequestQueues[queueName].Count == 2}
-                    });
-
-                    await ExecuteAsync(context,
-                        "vc clear");
-                    await AssertAsync(context, new()
-                    {
-                        {"song request queue is empty", config.CurrentSongRequestQueue.Count == 0},
-                        {"testing queue was unmodified", config.SavedSongRequestQueues[queueName].Count == 2}
-                    });
-
-                    await ExecuteAsync(context,
-                        $"vc pop-queue {queueName}");
-                    await AssertAsync(context, new()
-                    {
-                        {"testing queue does not exist", !config.SavedSongRequestQueues.ContainsKey(queueName)},
-                        {"song request queue is correct length", config.CurrentSongRequestQueue.Count == 2}
-                    });
-
-                    await ExecuteAsync(context,
-                        "vc clear");
-                }
+                "voice-queue",
+                VoiceQueueTest
             },
             {
-                "keywords", async (context) =>
-                {
-                    var config = KeywordsModule.GetConfigForGuild(context.Guild);
-                    const string keywordName = "testing_keyword";
-
-                    await ExecuteAsync(context,
-                        $"keywords remove {keywordName}");
-                    await AssertAsync(context,
-                        "keyword doesn't exist", !config.KeywordsDictionary.ContainsKey(keywordName));
-
-                    await ExecuteAsync(context,
-                        $"keywords add {keywordName} value1",
-                        $"keywords add \"{keywordName}\" \"value2 **Title!**with title\"");
-                    await AssertAsync(context, new()
-                    {
-                        {"keyword exists in dictionary", config.KeywordsDictionary.TryGetValue(keywordName, out List<KeywordValue> keywordValues)},
-                        {"check value1", keywordValues[0].Content == "value1"},
-                        {"check value2", keywordValues[1].Content == "value2 with title" && keywordValues[1].Title == "Title!"}
-                    });
-
-                    await ExecuteAsync(context,
-                        $"keywords remove {keywordName} value1");
-                    await AssertAsync(context,
-                        "value was removed", keywordValues.Count == 1);
-
-                    await ExecuteAsync(context,
-                        "keywords remove testing_keyword");
-                    await AssertAsync(context,
-                        "keyword was removed", !config.KeywordsDictionary.ContainsKey(keywordName));
-                }
+                "keywords",
+                KeywordsTest
             },
             {
-                // TODO: also test specified author
-                "quotes", async (context) =>
-                {
-                    string repeatedText = string.Concat(
-                        Enumerable.Repeat("This is a quote with a lot of text.", 10));
-
-                    var results = await ExecuteAsync(context,
-                        $"text quote \"{repeatedText}\"");
-                }
+                "quotes",
+                QuotesTest
             }
         };
+
+        private static async Task MessagesTest(ICommandContext context)
+        {
+            await new SuccessMessage("This is a success message.", "Success").SendAsync(context.Channel);
+            await new InfoMessage("This is an info message.", "Info").SendAsync(context.Channel);
+            await new WarningMessage("This is a warning message.", "Warning").SendAsync(context.Channel);
+            await new ErrorMessage("This is an error message.", "Error").SendAsync(context.Channel);
+            await new GenericMessage("This is a response message.", "Response").SendAsync(context.Channel);
+
+            var listOfFieldBuilders = new List<EmbedFieldBuilder>();
+            for (int i = 0; i < 46; i++)
+            {
+                listOfFieldBuilders.Add(new EmbedFieldBuilder()
+                {
+                    Name = $"Field title {i}",
+                    Value = "This is some description text."
+                });
+            }
+            await new GenericMessage("This is a response message with fields.", "Fields", listOfFieldBuilders, 2)
+                .SendAsync(context.Channel);
+        }
+
+        private static async Task AliasesTest(ICommandContext context)
+        {
+            var config = MainModule.GetConfigForGuild(context.Guild);
+            const string aliasName = "testing_alias";
+
+            await ExecuteAsync(context,
+                $"alias {aliasName} \"{config.CommandPrefix} help\"");
+            await AssertAsync(context, new()
+            {
+                { "key exists in dictionary", config.AliasesDictionary.ContainsKey(aliasName) },
+                { "check definition", config.AliasesDictionary[aliasName] == "help" }
+            });
+
+            await ExecuteAsync(context,
+                $"alias {aliasName}");
+            await AssertAsync(context,
+                "alias has been removed", !config.AliasesDictionary.ContainsKey(aliasName));
+        }
+
+        private static async Task VoiceTest(ICommandContext context)
+        {
+            // TODO: These Task.Delays are a bit of a hacky workaround.
+            //       Find some way to reliably wait until the command finishes with timeout.
+
+            var config = VoiceModule.GetConfigForGuild(context.Guild);
+
+            await ExecuteAsync(context,
+                "vc clear",
+                "vc skip",
+                "vc join");
+            await DelayAsync(context, 3000);
+            await AssertAsync(context, new()
+            {
+                { "song request queue is empty", config.CurrentSongRequestQueue.Count == 0 },
+                { "nothing is playing", config.CurrentlyPlayingSongRequest == null },
+                { "audio client has connected", !VoiceModule.CheckIfAudioClientDisconnected(config.AudioClient) }
+            });
+
+            await ExecuteAsync(context,
+                "vc add never gonna give you up",
+                "vc add https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                "vc add \"me at the zoo\"");
+            await DelayAsync(context, 10000);
+            await ExecuteAsync(context,
+                "vc list");
+            await AssertAsync(context, new()
+            {
+                { "currently playing is not null", config.CurrentlyPlayingSongRequest != null },
+                { "song request queue is correct length", config.CurrentSongRequestQueue.Count == 2 }
+            });
+
+            var firstCurrentlyPlayingRequest = config.CurrentlyPlayingSongRequest;
+
+            await ExecuteAsync(context,
+                "vc skip");
+            await AssertAsync(context, new()
+            {
+                { "check next request is playing", config.CurrentlyPlayingSongRequest != firstCurrentlyPlayingRequest }
+            });
+
+            await ExecuteAsync(context,
+                "vc clear");
+            await AssertAsync(context,
+                "song request queue is empty", config.CurrentSongRequestQueue.Count == 0);
+
+            await ExecuteAsync(context,
+                "vc leave");
+            await AssertAsync(context,
+                "audio client has disconnected", VoiceModule.CheckIfAudioClientDisconnected(config.AudioClient));
+        }
+
+        private static async Task VoiceQueueTest(ICommandContext context)
+        {
+            var config = VoiceModule.GetConfigForGuild(context.Guild);
+            const string queueName = "testing-queue";
+
+            await ExecuteAsync(context,
+                $"vc pop-queue {queueName}",
+                "vc clear");
+            await AssertAsync(context, new()
+            {
+                { "testing queue does not exist", !config.SavedSongRequestQueues.ContainsKey(queueName) },
+                { "song request queue is empty", config.CurrentSongRequestQueue.Count == 0 }
+            });
+
+            await ExecuteAsync(context,
+                "vc add https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                "vc add https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+            await DelayAsync(context, 10000);
+            await AssertAsync(context,
+                "song request queue is correct length", config.CurrentSongRequestQueue.Count == 2);
+
+            await ExecuteAsync(context,
+                $"vc save {queueName}");
+            await AssertAsync(context, new()
+            {
+                { "testing queue exists in saved queues", config.SavedSongRequestQueues.ContainsKey(queueName) },
+                { "correct number of songs in testing queue", config.SavedSongRequestQueues[queueName].Count == 2 }
+            });
+
+            await ExecuteAsync(context,
+                "vc clear");
+            await AssertAsync(context, new()
+            {
+                { "song request queue is empty", config.CurrentSongRequestQueue.Count == 0 },
+                { "testing queue was unmodified", config.SavedSongRequestQueues[queueName].Count == 2 }
+            });
+
+            await ExecuteAsync(context,
+                $"vc pop-queue {queueName}");
+            await AssertAsync(context, new()
+            {
+                { "testing queue does not exist", !config.SavedSongRequestQueues.ContainsKey(queueName) },
+                { "song request queue is correct length", config.CurrentSongRequestQueue.Count == 2 }
+            });
+
+            await ExecuteAsync(context,
+                "vc clear");
+        }
+
+        private static async Task KeywordsTest(ICommandContext context)
+        {
+            var config = KeywordsModule.GetConfigForGuild(context.Guild);
+            const string keywordName = "testing_keyword";
+
+            await ExecuteAsync(context,
+                $"keywords remove {keywordName}");
+            await AssertAsync(context,
+                "keyword doesn't exist", !config.KeywordsDictionary.ContainsKey(keywordName));
+
+            await ExecuteAsync(context,
+                $"keywords add {keywordName} value1",
+                $"keywords add \"{keywordName}\" \"value2 **Title!**with title\"");
+            await AssertAsync(context, new()
+            {
+                { "keyword exists in dictionary", config.KeywordsDictionary.TryGetValue(keywordName, out List<KeywordValue> keywordValues) },
+                { "check value1", keywordValues[0].Content == "value1" },
+                { "check value2", keywordValues[1].Content == "value2 with title" && keywordValues[1].Title == "Title!" }
+            });
+
+            await ExecuteAsync(context,
+                $"keywords remove {keywordName} value1");
+            await AssertAsync(context,
+                "value was removed", keywordValues.Count == 1);
+
+            await ExecuteAsync(context,
+                "keywords remove testing_keyword");
+            await AssertAsync(context,
+                "keyword was removed", !config.KeywordsDictionary.ContainsKey(keywordName));
+        }
+
+        private static async Task QuotesTest(ICommandContext context)
+        {
+            // TODO: also test specified author
+            string repeatedText = string.Concat(
+                Enumerable.Repeat("This is a quote with a lot of text.", 10));
+
+            var results = await ExecuteAsync(context,
+                $"text quote \"{repeatedText}\"");
+        }
 
         private static async Task<List<IResult>> ExecuteAsync(ICommandContext context, params string[] commands)
         {
