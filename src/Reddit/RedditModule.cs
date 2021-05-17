88c32898a0b82c3c1b62a2342a7c65a1e9b67d37
Rednir <davran.dilshat@gmail.com>
2021-05-17 07:56:11 +0100

    add method to get subreddit

--- src/Reddit/RedditModule.cs
+++ src/Reddit/RedditModule.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Threading.Tasks;
 using Discord.Commands;
+using Reddit;
+using Reddit.Controllers;
+using wow2.Data;
 
 namespace wow2.Modules
 {
@@ -10,11 +13,18 @@ namespace wow2.Modules
     [Summary("Integrations with Reddit.")]
     public class RedditModule : Module
     {
+        private static readonly RedditClient RedditClient = new(DataManager.Secrets.RedditAppId, DataManager.Secrets.RedditRefreshToken);
+
         [Command("top")]
         [Summary("Get the top post of a given subreddit")]
-        public async Task TopAsync(string subreddit)
+        public async Task TopAsync([Name("SUBREDDIT")] string subredditName)
         {
-            throw new NotImplementedException();
+            Subreddit subreddit = await GetSubredditAsync(subredditName);
+        }
+
+        public static async Task<Subreddit> GetSubredditAsync(string subreddit)
+        {
+            return RedditClient.Subreddit(subreddit);
         }
     }
 }
