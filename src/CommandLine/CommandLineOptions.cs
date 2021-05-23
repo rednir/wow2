60e5287a4df500a44d5d3d78b203ce86a74f8f67
Rednir <davran.dilshat@gmail.com>
2021-05-23 13:46:19 +0100

    add help option

--- src/CommandLine/CommandLineOptions.cs
+++ src/CommandLine/CommandLineOptions.cs
@@ -1,34 +1,55 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Text;
+using Discord;
 using wow2.Verbose;
 
 namespace wow2.CommandLine
 {
     public class CommandLineOptions
     {
+        public static IEnumerable<MethodInfo> OptionMethods { get; } = typeof(CommandLineOptions).GetMethods()
+            .Where(m => m.GetCustomAttributes(typeof(OptionAttribute), false).Length > 0);
+
         /// <summary>Executes a method with a matching attribute.</summary>
-        /// <returns>True if a matching method was found.</summary>
+        /// <returns>True if the user specified an option.</summary>
         public static bool ParseArgs(string[] args)
         {
             if (args.Length == 0)
                 return false;
 
-            var optionMethods = typeof(CommandLineOptions).GetMethods().Where(
-                m => m.GetCustomAttributes(typeof(OptionAttribute), false).Length > 0);
-
-            foreach (MethodInfo method in optionMethods)
+            foreach (MethodInfo method in OptionMethods)
             {
-                var attribute = (OptionAttribute)method.GetCustomAttribute(typeof(OptionAttribute));
+                var attribute = GetOptionAttribute(method);
                 if (args[0] == "--" + attribute.LongName || args[0] == "-" + attribute.ShortName)
                 {
+                    Logger.Log($"Found option '{attribute.LongName}'", LogSeverity.Debug);
                     var action = (Action)Delegate.CreateDelegate(typeof(Action), null, method);
                     action.Invoke();
                     return true;
                 }
             }
 
-            return false;
+            Logger.Log("Invalid option.", LogSeverity.Error);
+            return true;
         }
+
+        [Option("help", 'h')]
+        public void Help()
+        {
+            var stringBuilder = new StringBuilder("\nApplication options:\n");
+            foreach (MethodInfo method in OptionMethods)
+            {
+                var attribute = GetOptionAttribute(method);
+                stringBuilder.AppendLine($"  --{attribute.LongName}");
+            }
+
+            Console.WriteLine(stringBuilder.ToString());
+        }
+
+        private static OptionAttribute GetOptionAttribute(MethodInfo method) =>
+            (OptionAttribute)method.GetCustomAttribute(typeof(OptionAttribute));
     }
 }
