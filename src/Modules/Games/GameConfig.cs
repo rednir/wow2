2996643c7f0ddf3eefb0aa5063bbd5bc0f1fc279
Rednir <davran.dilshat@gmail.com>
2021-05-26 18:53:51 +0100

    json ignore properties

--- src/Modules/Games/GameConfig.cs
+++ src/Modules/Games/GameConfig.cs
@@ -1,11 +1,14 @@
+using System.Text.Json.Serialization;
 using Discord.Commands;
 
 namespace wow2.Modules.Games
 {
     public abstract class GameConfig
     {
+        [JsonIgnore]
         public ICommandContext InitalContext { get; set; }
 
+        [JsonIgnore]
         public bool IsGameStarted { get; set; }
     }
 }
