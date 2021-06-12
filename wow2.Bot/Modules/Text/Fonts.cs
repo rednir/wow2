cc41805f01c8666bc1b52a6fd0859d1f4fc39347
Rednir <davran.dilshat@gmail.com>
2021-06-12 09:54:55 +0100

    use font family with emojis

--- wow2.Bot/Modules/Text/Fonts.cs
+++ wow2.Bot/Modules/Text/Fonts.cs
@@ -8,12 +8,15 @@ namespace wow2.Bot.Modules.Text
     {
         private static readonly FontCollection FontCollection = new();
 
+        private static readonly FontFamily EmojiFontFamily = FontCollection.Install(
+            Resource.ResourceAssembly.GetManifestResourceStream("wow2.Resources.Fonts.OpenSansEmoji.ttf"));
+
         private static readonly FontFamily RegularFontFamily = FontCollection.Install(
             Resource.ResourceAssembly.GetManifestResourceStream("wow2.Resources.Fonts.ClearSans-Medium.ttf"));
 
         private static readonly FontFamily LightFontFamily = FontCollection.Install(
             Resource.ResourceAssembly.GetManifestResourceStream("wow2.Resources.Fonts.Cantarell-Light.ttf"));
 
-        public static Font QuoteTextFont => LightFontFamily.CreateFont(32, FontStyle.Regular);
+        public static Font QuoteTextFont => EmojiFontFamily.CreateFont(32, FontStyle.Regular);
     }
 }
