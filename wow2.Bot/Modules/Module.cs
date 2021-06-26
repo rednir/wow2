32b6625a174a5d8757131cf53170a8dbf68b8bb6
Rednir <davran.dilshat@gmail.com>
2021-06-26 15:08:36 +0100

    add toggle question method

--- wow2.Bot/Modules/Module.cs
+++ wow2.Bot/Modules/Module.cs
@@ -1,8 +1,35 @@
+using System;
+using System.Threading.Tasks;
 using Discord.Commands;
+using wow2.Bot.Data;
+using wow2.Bot.Verbose.Messages;
 
 namespace wow2.Bot.Modules
 {
     public abstract class Module : ModuleBase<SocketCommandContext>
     {
+        public async Task SendToggleQuestionAsync(
+            bool currentState,
+            Action<bool> setter,
+            string toggledOnMessage = "Toggled setting.",
+            string toggledOffMessage = "Toggled setting.")
+        {
+            await new QuestionMessage(
+                description: $"Are you sure you want to turn this setting {(!currentState ? "on" : "off")}?",
+                title: null,
+                onConfirm: async () =>
+                {
+                    setter.Invoke(!currentState);
+                    await DataManager.SaveGuildDataToFileAsync(Context.Guild.Id);
+                    await new SuccessMessage(!currentState ? toggledOnMessage : toggledOffMessage)
+                        .SendAsync(Context.Channel);
+                },
+                onDeny: async () =>
+                {
+                    await new SuccessMessage("Nothing was changed.")
+                        .SendAsync(Context.Channel);
+                })
+                .SendAsync(Context.Channel);
+        }
     }
 }
