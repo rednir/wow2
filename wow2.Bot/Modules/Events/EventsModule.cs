db8b957b55bcdc962283e242f8d1b83ab863b88f
Rednir <davran.dilshat@gmail.com>
2021-07-03 22:12:29 +0100

    implement set-announcements-channel

--- wow2.Bot/Modules/Events/EventsModule.cs
+++ wow2.Bot/Modules/Events/EventsModule.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Discord.Commands;
 using Discord.WebSocket;
 using wow2.Bot.Data;
+using wow2.Bot.Verbose.Messages;
 
 namespace wow2.Bot.Modules.Events
 {
@@ -35,7 +36,11 @@ namespace wow2.Bot.Modules.Events
         [Summary("Sets the channel that event notifications will be sent to.")]
         public async Task SetAnnouncementsChannelAsync(SocketTextChannel channel)
         {
-            throw new NotImplementedException();
+            Config.AnnouncementsChannelId = channel.Id;
+            await DataManager.SaveGuildDataToFileAsync(Context.Guild.Id);
+
+            await new SuccessMessage($"You'll get notified about events in {channel.Mention}")
+                .SendAsync(Context.Channel);
         }
     }
 }
--- wow2.Bot/Modules/Events/EventsModuleConfig.cs
+++ wow2.Bot/Modules/Events/EventsModuleConfig.cs
@@ -2,5 +2,6 @@ namespace wow2.Bot.Modules.Events
 {
     public class EventsModuleConfig
     {
+        public ulong AnnouncementsChannelId { get; set; }
     }
 }
