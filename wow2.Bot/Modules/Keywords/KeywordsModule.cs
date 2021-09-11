e44ec81d9a2e6f64316a83ebba5f2469b6068063
Davran Dilshat <davran.dilshat@gmail.com>
2021-09-11 23:30:28 +0100

    change param order

--- wow2.Bot/Modules/Keywords/KeywordsModule.cs
+++ wow2.Bot/Modules/Keywords/KeywordsModule.cs
@@ -182,7 +182,7 @@ namespace wow2.Bot.Modules.Keywords
         [Command("list")]
         [Alias("show", "all")]
         [Summary("Shows a list of all keywords, and a preview of their values. SORTBY can be date/likes/deletions/values")]
-        public async Task ListAsync(int page = 1, KeywordSorts sort = KeywordSorts.Date)
+        public async Task ListAsync(KeywordSorts sort = KeywordSorts.Date, int page = 1)
         {
             var keywordsDictionary = Config.KeywordsDictionary;
             var listOfFieldBuilders = new List<EmbedFieldBuilder>();
