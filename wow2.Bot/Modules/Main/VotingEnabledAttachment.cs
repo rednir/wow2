f490af0c182b20c0f5bf250e31dea88deb85daf9
Davran Dilshat <davran.dilshat@gmail.com>
2021-09-11 22:30:54 +0100

    add ctor

--- wow2.Bot/Modules/Main/VotingEnabledAttachment.cs
+++ wow2.Bot/Modules/Main/VotingEnabledAttachment.cs
@@ -1,14 +1,27 @@
 using System;
+using System.Linq;
+using Discord.Commands;
 
 namespace wow2.Bot.Modules.Main
 {
     public class VotingEnabledAttachment
     {
+        public VotingEnabledAttachment()
+        {
+        }
+
+        public VotingEnabledAttachment(SocketCommandContext context)
+        {
+            Link = context.Message.Attachments.FirstOrDefault()?.Url;
+            AuthorMention = context.User.Mention;
+            DateTime = context.Message.Timestamp;
+        }
+
         public string Link { get; set; }
 
         public string AuthorMention { get; set; }
 
-        public DateTime DateTime { get; set; }
+        public DateTimeOffset DateTime { get; set; }
 
         public int Likes { get; set; }
 
