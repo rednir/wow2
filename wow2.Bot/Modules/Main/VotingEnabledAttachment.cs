e556ea7a8447ec2d6e8cc3f1dcf6984eb2cbff1e
Davran Dilshat <davran.dilshat@gmail.com>
2021-09-11 22:54:17 +0100

    store more info about attachments

--- wow2.Bot/Modules/Main/VotingEnabledAttachment.cs
+++ wow2.Bot/Modules/Main/VotingEnabledAttachment.cs
@@ -12,12 +12,20 @@ namespace wow2.Bot.Modules.Main
 
         public VotingEnabledAttachment(SocketCommandContext context)
         {
-            Link = context.Message.Attachments.FirstOrDefault()?.Url;
+            ChannelId = context.Channel.Id;
+            MessageId = context.Message.Id;
+            FileName = context.Message.Attachments.FirstOrDefault()?.Url.Split('/').Last();
             AuthorMention = context.User.Mention;
             DateTime = context.Message.Timestamp;
         }
 
-        public string Link { get; set; }
+        public string AttachmentUrl => $"https://cdn.discordapp.com/attachments/{ChannelId}/{MessageId}/{FileName}";
+
+        public ulong ChannelId { get; set; }
+
+        public ulong MessageId { get; set; }
+
+        public string FileName { get; set; }
 
         public string AuthorMention { get; set; }
 
