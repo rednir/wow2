3374a83be35a529ba02975520c533921c61d01e2
Davran Dilshat <davran.dilshat@gmail.com>
2021-09-21 17:55:26 +0100

    add action buttons class

---
+++ wow2.Bot/Verbose/Messages/ActionButtons.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace wow2.Bot.Verbose.Messages
+{
+    public class ActionButtons
+    {
+        public string Label { get; set; }
+
+        public ButtonStyle Style { get; set; }
+
+        public string Url { get; set; }
+
+        public IEmote Emote { get; set; }
+
+        public bool Disabled { get; set; }
+
+        public int Row { get; set; }
+
+        public Func<SocketMessageComponent, Task> Action { get; set; }
+    }
+}
