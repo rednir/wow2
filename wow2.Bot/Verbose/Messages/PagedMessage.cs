78eebd33c988442a10f2abbf72d176b071be2b36
Davran Dilshat <davran.dilshat@gmail.com>
2021-09-21 19:10:52 +0100

    remove usings

--- wow2.Bot/Verbose/Messages/PagedMessage.cs
+++ wow2.Bot/Verbose/Messages/PagedMessage.cs
@@ -1,11 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Discord;
-using Discord.WebSocket;
-using wow2.Bot.Data;
-using wow2.Bot.Extensions;
 
 namespace wow2.Bot.Verbose.Messages
 {
