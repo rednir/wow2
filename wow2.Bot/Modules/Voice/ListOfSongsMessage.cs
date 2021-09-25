adf7e9eaa78bc4cdc239f1f2c60e23b00178c789
Davran Dilshat <davran.dilshat@gmail.com>
2021-09-25 13:09:39 +0100

    show total duration of songs

--- wow2.Bot/Modules/Voice/ListOfSongsMessage.cs
+++ wow2.Bot/Modules/Voice/ListOfSongsMessage.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Discord;
 using wow2.Bot.Verbose.Messages;
 
@@ -7,7 +8,7 @@ namespace wow2.Bot.Modules.Voice
     public class ListOfSongsMessage : PagedMessage
     {
         public ListOfSongsMessage(Queue<UserSongRequest> queue, string title = "List of songs", int? page = null)
-            : base(new List<EmbedFieldBuilder>(), string.Empty, title)
+            : base(new List<EmbedFieldBuilder>(), $"*The total duration of the songs below is {VoiceModule.DurationAsString(queue.Sum(r => r.VideoMetadata.duration))}*", title)
         {
             double totalDuration = 0;
             int i = 0;
