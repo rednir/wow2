980ff5c8e91c6f2747b169bf651c431f6bf778d5
Davran Dilshat <davran.dilshat@gmail.com>
2021-10-03 15:05:36 +0100

    reorder usings

--- wow2.Bot/Modules/Dev/DevModule.cs
+++ wow2.Bot/Modules/Dev/DevModule.cs
@@ -1,7 +1,7 @@
 using System;
-using System.Linq;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
