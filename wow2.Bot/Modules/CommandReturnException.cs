8b58f914a81581a4810cf4dd998dd2b28eb8d8d6
Davran Dilshat <davran.dilshat@gmail.com>
2021-10-22 16:24:39 +0100

    add empty ctor

--- wow2.Bot/Modules/CommandReturnException.cs
+++ wow2.Bot/Modules/CommandReturnException.cs
@@ -9,6 +9,10 @@ namespace wow2.Bot.Modules
     /// <summary>The exception that is thrown when a command returns with a warning message. Usually isn't fatal, only used to quickly return out of a command with a reply.</summary>
     public class CommandReturnException : Exception
     {
+        public CommandReturnException()
+        {
+        }
+
         public CommandReturnException(SocketCommandContext context, string description, string title = null)
             : base(title + description)
         {
