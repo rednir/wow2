6f158662220d7725c27241d06029c4cadafe46d6
Davran Dilshat <davran.dilshat@gmail.com>
2021-12-21 11:54:19 +0000

    order usings

--- wow2.Bot/Modules/Voice/VideoMetadata.cs
+++ wow2.Bot/Modules/Voice/VideoMetadata.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Web;
 using System.Xml;
-using System.Text.Json.Serialization;
 using Google.Apis.YouTube.v3.Data;
 using SpotifyAPI.Web;
 
